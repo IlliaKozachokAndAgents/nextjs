16d7253f715cb3a3db952a66a792c11ed9a36fd6
illiakozachok <iko@underdefense.com>
2026-05-30 12:35:22 +0400

    set icons and customize navbar

--- components/UI/header.tsx
+++ components/UI/header.tsx
@@ -1,54 +1,73 @@
-import {Link} from "@heroui/link";
-import {Button} from "@heroui/button";
-import {  Navbar,   NavbarBrand,   NavbarContent,   NavbarItem} from "@heroui/navbar";
+"use client"
 
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
+import { Button } from "@heroui/button";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const Logo = () => {
+    return <Image
+        src="/logo.png"
+        alt="Raise Hand"
+        width={26}
+        height={26}
+        priority
+    />;
 };
 
 export default function Header() {
-  return (
-    <Navbar>
-      <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
-  );
+    const pathname = usePathname()
+
+    const navItems = [
+        { href: "/", label: "MainPage" },
+        { href: "/test-link-1", label: "TestLink1" },
+        { href: "/test-link-2", label: "TestLink2" },
+    ]
+
+    return (
+        <Navbar>
+            <NavbarBrand>
+                <Link href="/" className="flex gap-1">
+                    <Logo />
+                    <p className="font-bold text-inherit">Raise Hand</p>
+                </Link>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                {
+                    navItems.map((item) => {
+                        const isActive = pathname === item.href
+
+                        return (
+                            <NavbarItem key={item.href}>
+                                <Link
+                                    color="foreground"
+                                    href={item.href}
+                                    className={`px-3 pu-1
+                                        ${isActive ? "text-blue-500" : "text-foreground"}
+                                        hover:text-blue-300 hover:border
+                                        hover:border-blue-300 hover:rounded-md
+                                        transition-colors
+                                        transition-border
+                                        duration-200
+                                    `}
+                                >
+                                    {item.label}
+                                </Link>
+                            </NavbarItem>)
+                    })
+                }
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Login</Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    );
 }
